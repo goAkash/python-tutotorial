ed6508b41eb5a52151222fefbf52b068b4e179a8
goAkash <ad48027@gmail.com>
2022-11-05 10:53:30 +0530

    day00

---
+++ subs.py
@@ -0,0 +1,10 @@
+# author:akash
+print("enter 1st number")
+value1=float(input())
+print("enter 2nd number")
+value2=float(input())
+print("enter 3rd number")
+value3=float(input())
+ans=value1 * value2 -value3
+print(ans4)
+
