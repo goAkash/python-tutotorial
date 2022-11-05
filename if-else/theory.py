9d6c4b986d3017a5111e9d05d755dbb3c8ac5abc
goAkash <ad48027@gmail.com>
2022-11-05 12:15:41 +0530

    jjkihb

---
+++ if-else/theory.py
@@ -0,0 +1,12 @@
+a = int(input());
+#we use if else when we have to determine ,multiple conditions.
+if a<0 :
+    print("Nuumber is negative")
+elif a>0 :
+    print("Positive Number")
+else :
+    print("0")
+
+# write a program to find maximum of three numbers.
+# take 3 input 
+# jo teeno me bada hai usko print kro.
