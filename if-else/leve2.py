ae11c644c627de25e740cfbd133758d35097da17
goAkash <ad48027@gmail.com>
2022-11-06 15:04:58 +0530

    greatest number

---
+++ if-else/leve2.py
@@ -0,0 +1,9 @@
+# author = akash deep
+print("enter 1st number")
+value1 =float(input())
+print("enter 2nd number")
+value2=float(input())
+if value1 > value2 :
+    print("value1 is greatest")
+else:
+    print("value2 is greatest")   
