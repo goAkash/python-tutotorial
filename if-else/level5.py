edf0dc9c970b0c7cb8a4cc916f2749f43e4dac77
goAkash <ad48027@gmail.com>
2022-11-07 12:08:50 +0530

    salary

---
+++ if-else/level5.py
@@ -0,0 +1,12 @@
+# author : akash deep
+print(" enter employ service years ")
+a = int(input())
+print(" enter employ salary per years ")
+b =int(input())
+if a >= 5 : 
+    print("your bonus amount is ", b * (5 / 100))
+    
+else :
+    print ("your bonus is zero" )  
+
+
