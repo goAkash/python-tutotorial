4db3fb1b380e25e61e7ff820b031cfaf6db4397d
goAkash <ad48027@gmail.com>
2022-11-07 20:44:48 +0530

    Day03

--- if-else/level6.py
+++ if-else/level6.py
@@ -1,14 +1,19 @@
-print("enter marks")
+print("Enter your marks to know your grade!\n")
 n =int(input())
-if n>25:
-    print("F")
-    25>= 45
-    print("E")
-    45>=50
-    print("D")
-    50>=60
-    print("C")
-    60>80
-    print("B")
+if n<25:
+    print("\"F\"")
+elif n<=45 and n>=25:
+    print("\"E\"")
+elif n<=50 and n>45:
+    print("\"D\"")
+elif n<=60 and n>50:
+    print("\"C\"")
+elif n<=80 and n>60:
+    print("\"B\"")
+else:
+    print("\"A\" \t Well done!")
+    
+
+
 
 
