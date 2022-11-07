aac89a2dc045ee507780636badf5495650dc0f03
goAkash <ad48027@gmail.com>
2022-11-07 21:36:56 +0530

    Adding day4

--- Loops/My_understanding.py
+++ Loops/My_understanding.py
@@ -1,2 +1,12 @@
-//For loop lecture  :
-//Loops are used to run the same code for multiple times.
+#For loop lecture  :
+#Loops are used to run the same code for multiple times.
+
+
+for akash in range(1,11,1) :
+    print(akash)
+
+print("\n \n ")
+i=1
+while i !=10:
+    print(i)
+    i=i+1
